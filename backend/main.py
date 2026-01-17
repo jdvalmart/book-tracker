c9a43dd2a12c5932155cdb37968572d1894e027a
Juan Valencia <juanvalencia9411@outlook.com>
2026-01-16 22:32:02 -0500

    ajuste al main

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Book Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://book-tracker1.netlify.app/"],  # OK para desarrollo
+    allow_origins=["https://book-tracker1.netlify.app"],  # OK para desarrollo
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
